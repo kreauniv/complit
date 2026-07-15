145599d2ef4e7161f28ef834000c48d2a3243599
Srikumar Subramanian <srikumar.subramanian@krea.edu.in>
2026-07-15 06:57:28 +0530

    Cricket pitch drawing program

--- source/src/cricket-pitch.rkt
+++ source/src/cricket-pitch.rkt
@@ -1,24 +1,30 @@
 #lang racket
 (require 2htdp/image)
 
+(define (front colour)
+  (rectangle 40 40 'solid colour))
+
+(define colour 'brown)
+
+
 (place-image (above
               (beside (rectangle 10 4 'solid 'red)
-                      (rectangle 3 4 'solid 'white)
+                      (rectangle 3 4 'solid 'transparent)
                       (rectangle 10 4 'solid 'red))              
-              (beside (rectangle 6 50 'solid 'brown)
-                      (rectangle 4 50 'solid 'white)
-                      (rectangle 6 50 'solid 'brown)
-                      (rectangle 4 50 'solid 'white)
-                      (rectangle 6 50 'solid 'brown)))
+              (beside (rectangle 6 50 'solid 'lightbrown)
+                      (rectangle 4 50 'solid 'transparent)
+                      (rectangle 6 50 'solid 'lightbrown)
+                      (rectangle 4 50 'solid 'transparent)
+                      (rectangle 6 50 'solid 'lightbrown)))
              40 325
              (above 
               (beside (rectangle 10 4 'solid 'red)
-                      (rectangle 3 4 'solid 'white)
+                      (rectangle 3 4 'solid 'transparent)
                       (rectangle 10 4 'solid 'red))              
-              (beside (rectangle 6 50 'solid 'brown)
-                      (rectangle 4 50 'solid 'white)
-                      (rectangle 6 50 'solid 'brown)
-                      (rectangle 4 50 'solid 'white)
-                      (rectangle 6 50 'solid 'brown))
+              (beside (rectangle 6 50 'solid 'lightbrown)
+                      (rectangle 4 50 'solid 'transparent)
+                      (rectangle 6 50 'solid 'lightbrown)
+                      (rectangle 4 50 'solid 'transparent)
+                      (rectangle 6 50 'solid 'lightbrown))
               (rectangle 80 300 'solid 'lightgreen)))
-                    
+
